8efe8ccbbd0470ced513e5b9585e6ee2f709c32f
Shiftrobin <ishefat@gmail.com>
2025-05-14 01:35:44 +0600

    domain change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['tech.noriyoshijp.com'] 
+        domains: ['api.noriyoshitech.com'] 
     }
 };
 
--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ='https://tech.noriyoshijp.com/api';
-const IMAGE_BASE_URL ='https://tech.noriyoshijp.com';
+const API_BASE_URL ='https://api.noriyoshitech.com/api';
+const IMAGE_BASE_URL ='https://api.noriyoshitech.com';
 
 export {API_BASE_URL, IMAGE_BASE_URL};
